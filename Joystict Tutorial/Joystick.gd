f5e91560c000732d49b36a588d8e4ebc4634a3b5
gamedevshirious <sad3119823@gmail.com>
2021-01-16 10:12:25 +0530

    Daily backup Sat, Jan 16, 2021 10:12:25 AM

--- Joystict Tutorial/Joystick.gd
+++ Joystict Tutorial/Joystick.gd
@@ -7,15 +7,17 @@ export var ease_value = 0.25
 export var joystick_range = 200
 var screensize = Vector2.ZERO
 var range_arr 
+var mouse_pos 
 
 func _ready():
 	screensize = get_viewport_rect().size
 	range_arr = find_range()
+	print($back.scale.x * 100)
+	print($back.scale.length_squared())
 
 func front_relative():
-	var mouse_pos = get_global_mouse_position()
-	
-	if Input.is_mouse_button_pressed(1):
+	mouse_pos = get_global_mouse_position()
+	if Input.is_mouse_button_pressed(1) and mouse_pos.x < screensize.x/2:
 		front.global_position = lerp(front.global_position, mouse_pos, ease_value)
 		front.global_position.x = clamp(front.global_position.x, range_arr[0], range_arr[1])
 		front.global_position.y = clamp(front.global_position.y, range_arr[2], range_arr[3])
