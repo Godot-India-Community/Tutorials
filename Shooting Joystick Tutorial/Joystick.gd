341c181ebad2c26042302dc6d8acdcc984ccbfeb
gamedevshirious <sad3119823@gmail.com>
2021-04-21 10:36:05 +0530

    Daily backup Wed, Apr 21, 2021 10:36:05 AM

--- Shooting Joystick Tutorial/Joystick.gd
+++ Shooting Joystick Tutorial/Joystick.gd
@@ -9,17 +9,22 @@ export var joystick_range = 75
 var screensize = Vector2.ZERO
 var range_arr 
 var mouse_pos 
+var event_pressed = false
 
 var mouse_in = false
 
 func _ready():
 	screensize = get_viewport_rect().size
 	range_arr = find_range()
+
+func _input(event):
+	if (event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed) or (event is InputEventScreenTouch and event.is_pressed()):
+		event_pressed = true
+	if (event is InputEventMouseButton and event.button_index == BUTTON_LEFT and not event.pressed) or (event is InputEventScreenTouch and not event.is_pressed()):	
+		event_pressed = false
 	
-func front_relative():
-	mouse_pos = get_global_mouse_position()
-	
-	if Input.is_mouse_button_pressed(1) and mouse_in:
+func front_relative(mouse_pos):
+	if event_pressed and mouse_in:
 		front.global_position = lerp(front.global_position, mouse_pos, ease_value)
 		front.global_position.x = clamp(front.global_position.x, range_arr[0], range_arr[1])
 		front.global_position.y = clamp(front.global_position.y, range_arr[2], range_arr[3])
@@ -27,7 +32,8 @@ func front_relative():
 		front.global_position = lerp(front.global_position, back.global_position, ease_value)
 
 func _process(delta):
-	front_relative()
+	mouse_pos = get_global_mouse_position()
+	front_relative(mouse_pos)
 
 func find_range():
 	var left_max_x = back.global_position.x - joystick_range
